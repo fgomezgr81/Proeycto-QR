bde7320f94db5796a8eb2c5d1b1285af91866ebb
fgomezgr81 <53118142+fgomezgr81@users.noreply.github.com>
2020-05-21 13:37:20 -0500

    m
    comi

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,84 +4,76 @@ import 'package:qrscan/pages/mapas_page.dart';
 import 'package:barcode_scan/barcode_scan.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
- int _currentPage =0;
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('QR Scanner'),
-        actions:<Widget>[
-          IconButton(
-            icon:Icon(Icons.delete_forever,color: Colors.red,size: 30,) ,
-            onPressed: (){},
-            ),
-        ]
-      ),
+      appBar: AppBar(title: Text('QR Scanner'), actions: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.delete_forever,
+            color: Colors.red,
+            size: 30,
+          ),
+          onPressed: () {},
+        ),
+      ]),
       body: _callPage(_currentPage),
       bottomNavigationBar: _btnNavigation(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.filter_center_focus),
-        onPressed:(){},
-        backgroundColor:Theme.of(context).primaryColor
-        ),
-        
+          child: Icon(Icons.filter_center_focus),
+          onPressed: _scanQR,
+          backgroundColor: Theme.of(context).primaryColor),
     );
   }
 
- Future _scanQR() async {
+  Future _scanQR() async {
+    dynamic futureString;
 
-  dynamic futureString ;
+    try {
+      futureString = await BarcodeScanner.scan();
+    } catch (e) {
+      futureString = e.toString();
+    }
 
-  try{
-    futureString = await BarcodeScanner.scan();
-  }catch(e){
-    futureString = e.toString();
-  }
+    print('future string : $futureString');
 
-  print('future string : $futureString');
-  
-
-  if(futureString != null){
-    print('tenemos informacion');
-  }
-
-}
-
-  Widget _callPage(int paginaActual){
-    switch(paginaActual){
-      case 0: return MapasPage();
-      case 1: return DireccionesPage();
-      default: return MapasPage();
+    if (futureString != null) {
+      print('tenemos informacion');
     }
   }
 
-Widget _btnNavigation(){
-  return BottomNavigationBar(
-    currentIndex: _currentPage,
-    onTap: (index){ 
-setState(() {
-  _currentPage=index;
-});
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+      default:
+        return MapasPage();
+    }
+  }
 
-    },
-    items: [
-      BottomNavigationBarItem(
-        icon:Icon(Icons.map),
-        title: Text('Mapas')
-        ),
+  Widget _btnNavigation() {
+    return BottomNavigationBar(
+      currentIndex: _currentPage,
+      onTap: (index) {
+        setState(() {
+          _currentPage = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.map), title: Text('Mapas')),
         BottomNavigationBarItem(
-        icon:Icon(Icons.brightness_5),
-        title: Text('Direccion')
-        )
-    ],
-  );
-}
+            icon: Icon(Icons.brightness_5), title: Text('Direccion'))
+      ],
+    );
+  }
 }
